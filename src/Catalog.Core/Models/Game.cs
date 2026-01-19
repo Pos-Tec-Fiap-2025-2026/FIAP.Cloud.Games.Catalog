3b99fea3f7e5927556a1dbd7f23f92bf80a58579
Lucas R. <pro.lukas@hotmail.com>
2026-01-19 18:15:09 -0300

    Passa valor fico true para a prop Active da classe Game.

--- src/Catalog.Core/Models/Game.cs
+++ src/Catalog.Core/Models/Game.cs
@@ -5,7 +5,7 @@ namespace Catalog.Core.Models
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string Description { get; set; } = string.Empty;
-        public bool Active { get; set; }
+        public bool Active { get; set; } = true;
 
         public virtual ICollection<Member> Members { get; set; } = [];
     }
